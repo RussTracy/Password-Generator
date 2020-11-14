544342193078c4721202b223934fba24da772d28
Russ <ocjade@comcast.net>
2020-11-14 11:37:27 -0700

    Random Password Feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
 // Assignment code here
+
+// Function to capture the proper password length or continue to prompt
+var pwLength = function() {
+  var lengthValue = "";
+  // prompt cannot be empty, be less than 8, more than 128 and cannot contain text
+  while (lengthValue === "" || lengthValue === null || parseInt(lengthValue) < 8 || parseInt(lengthValue) > 128 || isNaN(parseInt(lengthValue))){
+    lengthValue = prompt('Choose Password Length: 8 - 128');
+  }
+  console.log("password length is " + lengthValue)
+  return lengthValue;
+}
+
+
 function generatePassword(){
-  var newPassword = "Test";
-  return newPassword;
+   
+  var passwordLength =  pwLength(); // Length of Password Requestd
+  var passwordcharacters = "abcdefghijklmnopqrstuvwxyz"; // Type of Characters to be used in password
+  var newPassword = "" 
+
+for (var i = 0, password = passwordcharacters.length; i < passwordLength; ++i) {
+  newPassword += passwordcharacters.charAt(Math.floor(Math.random() * password))
+}
+ return newPassword;
 }
 
 // Get references to the #generate element
